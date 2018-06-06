0a3b3bdeef70497f3495321b1692d2f8f4f3d406
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-06 20:11:31 +0000

    Done.

---
+++ about_me.rb
@@ -0,0 +1,10 @@
+local_greeting = "Hey Dude."
+your_name = "Jacob"
+my_name = "Rylee"
+time_of_day = "summer time."
+
+def greeting
+  puts "#{local_greeting} #{your_name} and I'm #{my_name}, how's you #{time_of_day}?"
+end
+  
+  greeting
--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,59 @@
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+end 
+
+def base_footwork
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip step backward."
+end
+
+def skip_step(left)
+  puts "Lower left foot to the ground."
+  puts "Bounce left foot back up slightlym kicking it a few inches back."
+  puts "Left, Right, Left and Left."
+end
+
+def skip_step_right
+  puts "Lower right foot to the ground."
+  puts "Bounce right foot back up slightly, kicking it a few inches back."
+  puts "Right, Left, Right and Right."
+end
+
+def bob_the_reins
+  puts "Hold your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left wrist and hold them together."
+  puts "Lift your arms up and down in a loose bouncing movment, in time with the beat of the song. This movement is repeated eight times."
+end
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your let elbow straight left."
+  puts "Lift right arm up amd point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it ub small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+def bust_a_move
+  puts "Put right hand on forhead and salute."
+end
+
+starting_stance
+skip_step_right
+skip_step(left)
+base_footwork
+bob_the_reins
+lasso
+bust_a_move
+
+def gangnam_style
+starting_stance
+bust_a_move
+base_footwork
+bob_the_reins 
+skip_step_right 
+skip_step(left)
+lasso 
+skip_step_right
+skip_step(left)
+end 
 
 # Code your methods below
